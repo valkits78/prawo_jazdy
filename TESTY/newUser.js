5efba331fde389d84e908e421a22ec5bfc5bd8cf
ChatotPl <cw50953@zut.edu.pl>
2023-07-13 11:21:21 +0200

    Wstępna komunikacja

--- TESTY/newUser.js
+++ TESTY/newUser.js
@@ -1,57 +1,76 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
-app.post('/addUser', function (req, res) {
-  var user = {
-    "name": req.body.name,
-    "password": req.body.password,
-    "profession": req.body.profession,
-    "id": generateId() // Dodatkowo generuje unikalne ID dla nowego użytkownika
-  };
+const usersFilePath = path.join(__dirname, 'users.json');
 
-  fs.readFile("users.json", 'utf8', function (err, data) {
+app.get('/listUsers', function (req, res) {
+  fs.readFile(usersFilePath, 'utf8', function (err, data) {
     if (err) {
       console.log(err);
-      return res.status(500).send("Błąd serwera");
+      return res.status(500).send('Błąd serwera');
     }
-
-    var users = {};
 
     try {
-      users = JSON.parse(data);
+      const users = JSON.parse(data);
+      res.json(users);
     } catch (error) {
       console.log(error);
-      return res.status(500).send("Nieprawidłowy format pliku JSON");
+      return res.status(500).send('Nieprawidłowy format pliku JSON');
+    }
+  });
+});
+
+app.post('/addUser', function (req, res) {
+  const user = {
+    name: req.body.name,
+    password: req.body.password,
+    profession: req.body.profession,
+    id: generateId()
+  };
+
+  fs.readFile(usersFilePath, 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Błąd serwera');
     }
 
-    users["user" + user.id] = user;
+    try {
+      const users = JSON.parse(data);
+      users['user' + user.id] = user;
 
-    fs.writeFile("users.json", JSON.stringify(users), function (err) {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("Błąd serwera");
-      }
+      fs.writeFile(usersFilePath, JSON.stringify(users), function (err) {
+        if (err) {
+          console.log(err);
+          return res.status(500).send('Błąd serwera');
+        }
 
-      console.log("Użytkownik został dodany: ", user);
-      res.redirect("/");
-    });
+        console.log('Użytkownik został dodany:', user);
+        //res.json({ success: true, user });
+		res.redirect('/');
+		
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send('Nieprawidłowy format pliku JSON');
+    }
   });
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
-var server = app.listen(8081, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Serwer nasłuchuje na http://%s:%s", host, port);
+const server = app.listen(8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Serwer nasłuchuje na http://${host}:${port}`);
 });
 
-// Funkcja do generowania unikalnego ID
 function generateId() {
   return Math.floor(Math.random() * 1000);
 }
