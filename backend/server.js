2532d336583f660224e08bcdb952afd6cbe052c5
Aleksander Śpiewak <sa51127@zut.edu.pl>
2023-07-20 09:12:40 +0200

    obsługa logowania

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const session = require('express-session');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
 const port = 3000;
+const dbPath = 'dl.db';
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +14,9 @@ app.use(express.json());
 // Ustalenie katalogu, w którym znajdują się pliki HTML i CSS
 const staticDir = path.join(__dirname, '../frontend');
 app.use(express.static(staticDir));
+
+// Tworzenie połączenia z bazą danych
+const db = new sqlite3.Database(dbPath);
 
 // Ustawienia sesji
 app.use(session({
@@ -35,25 +41,36 @@ app.get('/login', (req, res) => {
 
 // Obsługa logowania
 app.post('/login', (req, res) => {
-    // Sprawdzenie danych logowania
-    const username = req.body.login;
-    const password = req.body.password;
-  
-    
-    // Walidacja użytkownika (przykładowe dane)
-    if (username === 'admin' && password === 'password') {
-      req.session.isLoggedIn = true;
-      res.redirect('/city');
+  // Sprawdzenie danych logowania
+  const username = req.body.login;
+  const password = req.body.password;
+
+  const query = 'SELECT * FROM user';
+
+  db.all(query, [], (err, rows) => {
+    if (err) { 
+      // Obsługa błędu w przypadku niepowodzenia zapytania
+      console.error(err);
+      res.status(500).send('Internal Server Error');
     } else {
-      res.redirect('/login');
+      const user = rows.find((row) => row.login === username && row.password === password);
+      if (user) {
+        req.session.isLoggedIn = true;
+        res.redirect('/city');
+      } else {
+        res.redirect('/login');
+      }
     }
   });
+});
+    
+   
+      
+
 
 app.get('/city', checkAuth, (req, res) => {
     res.sendFile(path.join(staticDir, '../frontend/city.html'));
 });
-
-
 
 
 
